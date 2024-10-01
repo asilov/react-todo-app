09f6cda2ea9020d9a981ae4f8ffec3834602dc6c
Asilbek Asilov <asil.california@gmail.com>
2024-10-01 00:14:49 -0700

    Create forms and submit button

--- src/Todo.js
+++ src/Todo.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './styles.css'
 
 function Todo() {
-  return (
-    <div>Todo</div>
-  )
+	const [todos, setTodos] = useState([])
+	const [input, setInput] = useState('')
+
+	const handleSubmit = () => {
+		setTodos((todos) => {
+			todos.concat({ text: input, id: generateId() })
+		})
+	}
+
+	return (
+		<>
+			<div className='container'>
+				<input
+					type='text'
+					value={input}
+					onChange={(e) => setInput(e.target.value)}
+					placeholder='New Todo'
+				/>
+				<button onClick={handleSubmit}>Submit</button>
+			</div>
+		</>
+	)
 }
 
 export default Todo
